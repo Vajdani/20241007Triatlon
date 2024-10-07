fab29ea2f2996470cc14aaf2f333f55b27fbd3fa
Vajdani <50171679+Vajdani@users.noreply.github.com>
2024-10-07 10:07:14 +0200

    Upload project

---
+++ vajda_daniel_triatlon/vajda_daniel_triatlon/Program.cs
@@ -0,0 +1,30 @@
+﻿using vajda_daniel_triatlon;
+
+List<Versenyzo> versenyzok = [];
+
+StreamReader reader = new(path: "../../../forras.txt", encoding: System.Text.Encoding.UTF8);
+while (!reader.EndOfStream)
+{
+    versenyzok.Add(new Versenyzo(reader.ReadLine()!));
+}
+reader.Close();
+
+Console.WriteLine($"{versenyzok.Count} versenyző fejezte be a versenyt.");
+Console.WriteLine($"{versenyzok.Count(p => p.Kategoria == "elit")} versenyző volt az 'elit' kategóriában.");
+Console.WriteLine($"A női versenyzők átlagéletkora: {versenyzok.Where(p => !p.Nem).Average(p => DateTime.Now.Year - p.SzuletesiEv):0.00} év");
+Console.WriteLine($"A versenyzők kerékpározással töltött összideje: {versenyzok.Sum(p => p.KerekparIdo.TotalSeconds)/60/60:0.00} óra");
+Console.WriteLine($"Átlagos úszási idő elit junior kategóriában: {TimeSpan.FromSeconds(versenyzok.Where(p => p.Kategoria == "elit junior").Average(p => p.UszasIdo.TotalSeconds))}");
+Console.WriteLine($"Férfi győztes: {versenyzok.Where(p => p.Nem).MinBy(p => (p.UszasIdo + p.Depo1Ido + p.KerekparIdo + p.Depo2Ido + p.FutasIdo).TotalSeconds)}");
+
+Console.WriteLine($"A versenyzők kategóriánként:");
+var kategoriak = versenyzok.OrderBy(p => p.Kategoria).GroupBy(p => p.Kategoria);
+foreach (var item in kategoriak)
+{
+    Console.WriteLine($"\t{item.Key}: {item.Count()}");
+}
+
+Console.WriteLine($"Kategóriánként az átlagos depóban töltött idő:");
+foreach (var item in kategoriak)
+{
+    Console.WriteLine($"\t{item.Key}: {TimeSpan.FromSeconds(item.Average(p => (p.Depo1Ido + p.Depo1Ido).TotalSeconds))}");
+}
---
+++ vajda_daniel_triatlon/vajda_daniel_triatlon/Versenyzo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vajda_daniel_triatlon
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int SzuletesiEv { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Nem { get; set; }
+        public string Kategoria { get; set; }
+
+        public TimeSpan UszasIdo { get; set; }
+        public TimeSpan Depo1Ido { get; set; }
+        public TimeSpan KerekparIdo { get; set; }
+        public TimeSpan Depo2Ido { get; set; }
+        public TimeSpan FutasIdo { get; set; }
+
+        public Versenyzo(string line)
+        {
+            string[] split = line.Split(';');
+            Nev = split[0];
+            SzuletesiEv = int.Parse(split[1]);
+            Rajtszam = int.Parse(split[2]);
+            Nem = split[3] == "f";
+            Kategoria = split[4];
+
+            UszasIdo = TimeSpan.Parse(split[5]);
+            Depo1Ido = TimeSpan.Parse(split[6]);
+            KerekparIdo = TimeSpan.Parse(split[7]);
+            Depo2Ido = TimeSpan.Parse(split[8]);
+            FutasIdo = TimeSpan.Parse(split[9]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev} (Született: {SzuletesiEv}; Rajtszám: {Rajtszam}; Kategória: {Kategoria})";
+        }
+    }
+}
